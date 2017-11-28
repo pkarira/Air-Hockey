3614ff960dfebadc6e06bfcd52605860991b504d
pkarira <pulkit5208@gmail.com>
2017-11-29 01:27:59 +0530

    added functionality for ball collision

--- scripts/components.js
+++ scripts/components.js
@@ -4,18 +4,47 @@ this.x = x;
 this.y = y;
 this.context=context;
 this.radius = radius;
+this.color=color;
 this.update = function(){
-      this.context.fillStyle = color;
+      this.context.fillStyle = this.color;
       this.context.arc(this.x,this.y,this.radius,0,2*Math.PI);
       this.context.fill();
     }
 }
-function goalPost(width , color,context,canvasWidth,canvasHeight)
+function goalPost(width,color,context,canvasWidth,canvasHeight)
 {
-  context.lineWidth=100;
-  context.moveTo(canvasWidth/2-width/2,0);
-  context.lineTo(canvasWidth/2+width/2,0);
-  context.moveTo(canvasWidth/2-width/2,canvasHeight);
-  context.lineTo(canvasWidth/2+width/2,canvasHeight);
-  context.stroke();
+  this.width = width;
+  this.canvasWidth = canvasWidth;
+  this.canvasHeight = canvasHeight;
+  this.context=context;
+  this.color=color;
+  this.drawLine=function()
+  {
+  this.context.lineWidth=10;
+  this.context.moveTo(this.canvasWidth/2-this.width/2,0);
+  this.context.lineTo(this.canvasWidth/2+this.width/2,0);
+  this.context.stroke();
+  this.context.moveTo(this.canvasWidth/2-this.width/2,this.canvasHeight);
+  this.context.lineTo(this.canvasWidth/2+this.width/2,this.canvasHeight);
+  this.context.stroke();
+}
+}
+function hockey(radius, color, x, y,context,fieldColor)
+{
+  this.x = x;
+  this.y = y;
+  this.context=context;
+  this.radius = radius;
+  this.color=color;
+  this.fieldColor=fieldColor;
+  this.update = function(x,y){
+        this.context.fillStyle = this.fieldColor;
+        this.context.arc(this.x,this.y,this.radius,0,2*Math.PI);
+        this.context.fill();
+        this.x = x;
+        this.y = y;
+        this.context.fillStyle = this.color;
+        this.context.arc(this.x,this.y,this.radius,0,2*Math.PI);
+        this.context.fill();
+      }
 }
--- scripts/game_view.js
+++ scripts/game_view.js
@@ -1,21 +1,49 @@
 var canvas=document.getElementById('canvas');
 var canvasWidth=500;
 var canvasHeight=600;
+var hockeyRadius=20;
 canvas.width= canvasWidth;
 canvas.height= canvasHeight;
-var ballRadius=30;
+var ballRadius=20;
 var xDirection=1;
 var yDirection=1;
+var lastX,lastY,newX,newY;
+//var canvasMarginRight=screen.width/2-canvasWidth/2;
+var canvasMarginRight=300;
+var canvasMargintop=30;
 var context=canvas.getContext("2d");
-var ball=new ball(ballRadius, "red", 30, 30,context);
-var goalPost=new goalPost(canvasWidth/2, "red", context,canvasWidth,canvasHeight);
+var ball=new ball(ballRadius, "red", 20, 20,context);
+var goalPost=new goalPost(canvasWidth/2, "black", context,canvasWidth,canvasHeight);
+var hockey=new hockey(hockeyRadius, "red", 20, 20,context,"white");
+//goalPost.drawLine();
+canvas.addEventListener('mousemove', function (e) {
+          newX=e.pageX-canvasMarginRight;
+          newY=e.pageY-canvasMargintop;
+          hockey.update(newX,newY);
+          if(Math.abs(ball.x-hockey.x)<=2*ballRadius && Math.abs(ball.y-hockey.y)<=2*ballRadius)
+          {
+            ballHockeyCollision();}
+          lastX=e.pageX-canvasMarginRight;
+          lastY=e.pageY-canvasMargintop;
+        })
 ball.update();
 setInterval(updateGameArena,10);
 function updateGameArena() {
   context.clearRect(0, 0, canvas.width,canvas.height);
   context.beginPath();
-  ball.x+=xDirection;
-  ball.y+=yDirection;
+  //goalPost.drawLine();
+  // if(ball.x+ballRadius+xDirection>canvasWidth-ball.x)
+  // xDirection=canvasWidth-ball.x-2;
+  // else
+  // if(ball.x-ballRadius-xDirection<0)
+  // xDirection=ball.x-ballRadius-xDirection+2;
+  //
+  // if(ball.y+ballRadius+yDirection>canvasWidth-ball.y)
+  // yDirection=canvasWidth-ball.y-2;
+  // else
+  // if(ball.y-ballRadius-yDirection<0)
+  // yDirection=ball.y-ballRadius-yDirection+2;
+
   if(ball.x+ballRadius === canvasWidth)
   xDirection=-1;
   else
@@ -26,5 +54,15 @@ function updateGameArena() {
   else
   if(ball.y-ballRadius === 0)
   yDirection=1;
+  ball.x+=xDirection;
+  ball.y+=yDirection;
   ball.update();
-  }
+}
+function ballHockeyCollision()
+{
+  var xDiff=newX-lastX;
+  var yDiff=newY-lastY;
+  console.log("collision");
+  xDirection=xDiff;
+  yDirection=yDiff;
+}
