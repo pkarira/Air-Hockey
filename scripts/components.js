2ea1d48fe6ea97b561db49d75fa0c4d37ad7b4cc
pkarira <pulkit5208@gmail.com>
2017-11-29 22:33:08 +0530

    made private variables

--- scripts/components.js
+++ scripts/components.js
@@ -2,49 +2,49 @@ function ball(radius, color, x, y,context)
 {
 this.x = x;
 this.y = y;
-this.context=context;
-this.radius = radius;
-this.color=color;
+var context=context;
+var radius = radius;
+var color=color;
 this.update = function(){
-      this.context.fillStyle = this.color;
-      this.context.arc(this.x,this.y,this.radius,0,2*Math.PI);
-      this.context.fill();
+      context.fillStyle = this.color;
+      context.arc(this.x,this.y,this.radius,0,2*Math.PI);
+      context.fill();
     }
 }
 function goalPost(width,color,context,canvasWidth,canvasHeight)
 {
-  this.width = width;
-  this.canvasWidth = canvasWidth;
-  this.canvasHeight = canvasHeight;
-  this.context=context;
-  this.color=color;
-  this.drawLine=function()
+  var width = width;
+  var canvasWidth = canvasWidth;
+  var canvasHeight = canvasHeight;
+  var context=context;
+  var color=color;
+  var drawLine=function()
   {
-  this.context.lineWidth=10;
-  this.context.moveTo(this.canvasWidth/2-this.width/2,0);
-  this.context.lineTo(this.canvasWidth/2+this.width/2,0);
-  this.context.stroke();
-  this.context.moveTo(this.canvasWidth/2-this.width/2,this.canvasHeight);
-  this.context.lineTo(this.canvasWidth/2+this.width/2,this.canvasHeight);
-  this.context.stroke();
+  context.lineWidth=10;
+  context.moveTo(this.canvasWidth/2-this.width/2,0);
+  context.lineTo(this.canvasWidth/2+this.width/2,0);
+  context.stroke();
+  context.moveTo(this.canvasWidth/2-this.width/2,this.canvasHeight);
+  context.lineTo(this.canvasWidth/2+this.width/2,this.canvasHeight);
+  context.stroke();
 }
 }
 function hockey(radius, color, x, y,context,fieldColor)
 {
   this.x = x;
   this.y = y;
-  this.context=context;
-  this.radius = radius;
-  this.color=color;
-  this.fieldColor=fieldColor;
+  var context=context;
+  var radius = radius;
+  var color=color;
+  var fieldColor=fieldColor;
   this.update = function(x,y){
-        this.context.fillStyle = this.fieldColor;
-        this.context.arc(this.x,this.y,this.radius,0,2*Math.PI);
-        this.context.fill();
+        context.fillStyle = this.fieldColor;
+        context.arc(this.x,this.y,this.radius,0,2*Math.PI);
+        context.fill();
         this.x = x;
         this.y = y;
-        this.context.fillStyle = this.color;
-        this.context.arc(this.x,this.y,this.radius,0,2*Math.PI);
-        this.context.fill();
+        context.fillStyle = this.color;
+        context.arc(this.x,this.y,this.radius,0,2*Math.PI);
+        context.fill();
       }
 }
