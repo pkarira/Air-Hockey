0fb4a435fb91e015c6008f92386aae23ab2c1d91
pkarira <pulkit5208@gmail.com>
2017-12-07 03:20:56 +0530

    added database

--- app/controllers/game_controller.js
+++ app/controllers/game_controller.js
@@ -1,7 +1,18 @@
 var room;
+// for postgres connectin:- psql user_name  -h 127.0.0.1 -d db_name
+// for postgres refer codementor articles
+var pg = require('pg');
+var format = require('pg-format')
+var conString = "postgres://pulkit:pulkit@localhost:5432/pulkit";
+var client = new pg.Client(conString);
+client.connect();
 module.exports.getRoom=function(request,response)
 {
   room=request.body.room;
+  var ageQuery = format('SELECT * FROM rooms WHERE id = %L', "1")
+  const query = client.query(ageQuery, (err, res) => {
+    console.log(res.rows);
+});
   response.send("done");
 };
 module.exports.getGameArena=function(request,response)
